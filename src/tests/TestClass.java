ab8a1a4e0870e6c85ec33c8fbb64191b98f7b8ee
Silvan Adrian <hallo@silvanadrian.ch>
2015-04-06 22:07:05 +0200

    changed build.xml

--- src/tests/TestClass.java
+++ src/tests/TestClass.java
@@ -1,12 +1,14 @@
 package tests;
 
+import static org.junit.Assert.*;
+
 import org.junit.*;
 
 public class TestClass {
 	
 	@Test
-	public void testTrue() {
-		assert(true);
-	}
+	public void mySimpleTest(){
+        Assert.assertEquals(  2,  1 + 1  );
+    }
 
 }
