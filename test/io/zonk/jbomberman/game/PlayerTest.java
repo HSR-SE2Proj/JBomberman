02047035647c7ecd81e5868541d8b153854f3e28
Silvan Adrian <sadrian@hsr.ch>
2015-04-27 15:42:05 +0200

    added Tests for get/setBomberman

--- test/io/zonk/jbomberman/game/PlayerTest.java
+++ test/io/zonk/jbomberman/game/PlayerTest.java
@@ -1,10 +1,11 @@
 package io.zonk.jbomberman.game;
 
 import static org.junit.Assert.*;
-
+import io.zonk.jbomberman.game.server.GBomberman;
 import org.junit.*;
 public class PlayerTest {
 	private Player player;
+	private GBomberman gb;
 	@Before
 	public void init() {
 		player = new Player("Test",0);
@@ -18,5 +19,22 @@ public class PlayerTest {
 	@Test
 	public void testName() {
 		assertEquals("Test",player.getName());
+	}
+	
+	@Test
+	public void testGetBomberman() {
+		assertEquals(0,player.getBomberman().getId());
+		assertEquals(GameObjectType.BOMBERMAN,player.getBomberman().getType());
+		//anpassen, wenn Position mitgegeben wird
+		//assertEquals(null,player.getBomberman().getPosition());
+	}
+	
+	@Test
+	public void testSetBomberman() {
+		gb = new GBomberman(null, 1);
+		player.setBomberman(gb);
+		assertEquals(1,player.getBomberman().getId());
+		assertEquals(null,player.getBomberman().getPosition());
+		assertEquals(GameObjectType.BOMBERMAN,player.getBomberman().getType());
 	}
 }
