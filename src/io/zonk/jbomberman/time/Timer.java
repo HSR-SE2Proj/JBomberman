99abf90540453bd0228ac25a5a468e54871d86a1
Pascal Kistler <pascal-kistler@hotmail.com>
2015-04-27 13:37:12 +0200

    Reduced CPU load of Timer

--- src/io/zonk/jbomberman/time/Timer.java
+++ src/io/zonk/jbomberman/time/Timer.java
@@ -3,10 +3,10 @@ package io.zonk.jbomberman.time;
 import io.zonk.jbomberman.game.GameLoop;
 
 public class Timer extends Thread {
-	
+
 	private GameLoop loop;
 	private int millis;
-	
+
 	public Timer(int millis, GameLoop loop) {
 		this.loop = loop;
 		this.millis = millis;
@@ -14,18 +14,27 @@ public class Timer extends Thread {
 
 	@Override
 	public void run() {
-		long lastTime = System.currentTimeMillis();
-		long now;
-		long delta;
+// 		long lastTime = System.currentTimeMillis();
+// 		long now;
+// 		long delta;
 		System.out.println("[*] Timer started");
-		while(true) {
-			now = System.currentTimeMillis();
-			delta = now - lastTime;
-			if(delta >= millis) {
-				//System.out.println("Delta: " + (delta - millis));
-				loop.loop();
-				lastTime = System.currentTimeMillis();
+		while (true) {
+			Object monitoredObject = new Object();
+			synchronized (monitoredObject) {
+				try {
+					monitoredObject.notifyAll();
+					monitoredObject.wait(millis);
+				} catch (InterruptedException e) {
+				}
 			}
+			loop.loop();
+// 			now = System.currentTimeMillis();
+// 			delta = now - lastTime;
+// 			if(delta >= millis) {
+// 				//System.out.println("Delta: " + (delta - millis));
+// 				loop.loop();
+// 				lastTime = System.currentTimeMillis();
+// 			}
 		}
 	}
 }
