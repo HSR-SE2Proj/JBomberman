e760eae519afca66358b8dd96ed4cfcad4b1e812
Silvan Adrian <hallo@silvanadrian.ch>
2015-04-28 16:22:19 +0200

    changed Tests according to changes

--- test/io/zonk/jbomberman/game/PlayerTest.java
+++ test/io/zonk/jbomberman/game/PlayerTest.java
@@ -3,12 +3,16 @@ package io.zonk.jbomberman.game;
 import static org.junit.Assert.*;
 import io.zonk.jbomberman.game.server.GBomberman;
 import org.junit.*;
+import io.zonk.jbomberman.utils.Position;
 public class PlayerTest {
 	private Player player;
 	private GBomberman gb;
+	private Position pos;
 	@Before
 	public void init() {
 		player = new Player("Test",0);
+		pos = new Position(1,2);
+		gb = new GBomberman(pos, 1);
 	}
 	
 	@Test
@@ -22,19 +26,11 @@ public class PlayerTest {
 	}
 	
 	@Test
-	public void testGetBomberman() {
-		assertEquals(0,player.getBomberman().getId());
-		assertEquals(GameObjectType.BOMBERMAN,player.getBomberman().getType());
-		//anpassen, wenn Position mitgegeben wird
-		//assertEquals(null,player.getBomberman().getPosition());
-	}
-	
-	@Test
-	public void testSetBomberman() {
-		gb = new GBomberman(null, 1);
+	public void testGetSetBomberman() {
 		player.setBomberman(gb);
 		assertEquals(1,player.getBomberman().getId());
-		assertEquals(null,player.getBomberman().getPosition());
+		assertEquals(pos.getX(),player.getBomberman().getPosition().getX());
+		assertEquals(pos.getY(),player.getBomberman().getPosition().getY());
 		assertEquals(GameObjectType.BOMBERMAN,player.getBomberman().getType());
 	}
 }
