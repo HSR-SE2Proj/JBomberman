21a57f5e074b65109950966953dc7055a2b8caf0
Silvan Adrian <sadrian@hsr.ch>
2015-05-06 14:04:39 +0200

    ActionTest

---
+++ test/io/zonk/jbomberman/game/ActionTest.java
@@ -0,0 +1,27 @@
+package io.zonk.jbomberman.game;
+
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ActionTest {
+	private Action action;
+	private Object[] props;
+	
+	@Before
+	public void init(){
+		props = new Object[]{"Hello","Test"};
+		action = new Action(ActionType.MOVEMENT,props);
+	}
+	
+	@Test
+	public void testActionType(){
+		assertEquals(ActionType.MOVEMENT,action.getActionType());
+	}
+	
+	@Test
+	public void testGetProperty(){
+		assertEquals("Hello",action.getProperty(0));
+	}
+
+}
