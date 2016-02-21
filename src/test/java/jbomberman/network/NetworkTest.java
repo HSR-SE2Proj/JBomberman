0bd854cbacccf7cb40e1ce96a0a300cb9cdf494d
Silvan Adrian <hallo@silvanadrian.ch>
2016-02-21 17:58:59 +0100

    NetworkTests + Travis Service Rabbitmq

---
+++ src/test/java/jbomberman/network/NetworkTest.java
@@ -0,0 +1,46 @@
+package jbomberman.network;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import org.junit.*;
+
+import jbomberman.network.client.ClientNetwork;
+import jbomberman.network.server.ServerNetwork;
+import junit.framework.AssertionFailedError;
+
+
+public class NetworkTest {
+
+	private ClientNetwork clientNetwork;
+	private ServerNetwork serverNetwork;
+	@Before
+	public void setUp() {
+		clientNetwork = new ClientNetwork();
+		clientNetwork.connect("localhost");
+		serverNetwork = new ServerNetwork();
+		serverNetwork.connect("Localhost");
+	}
+	
+	@Test
+	public void testConnect() {
+		assertTrue(clientNetwork.isOpen());
+		assertTrue(serverNetwork.isOpen());
+	}
+	
+	@Test
+	public void testSendMessagefromClient() {
+		byte[] message = {2,3,4};
+		clientNetwork.sendMessage(message);
+		assertArrayEquals(message,serverNetwork.receiveMessage());
+	}
+	
+	@Test
+	public void testSendMessagefromServer() {
+		byte[] message = {2,3,4};
+		serverNetwork.sendMessage(message);
+		assertArrayEquals(message,clientNetwork.receiveMessage());
+	}
+}
