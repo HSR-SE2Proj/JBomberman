014315b90200207c941f11eb29710e40ada6bdb9
Silvan Adrian <hallo@silvanadrian.ch>
2016-02-26 18:13:24 +0100

    more ScorePanel Tests

--- src/test/java/jbomberman/view/ScorePanelTest.java
+++ src/test/java/jbomberman/view/ScorePanelTest.java
@@ -9,16 +9,26 @@ import org.junit.Before;
 import org.junit.Test;
 
 import jbomberman.game.Party;
+import jbomberman.game.Player;
 
 public class ScorePanelTest {
 	
 	private ScorePanel panel;
 	private Party party;
+	private Player player1,player2,player3,player4;
 	
 	@Before
 	public void setUp() {
 		party = new Party();
 		panel = new ScorePanel(party,1);
+		player1 = new Player("Player1",1);
+		player2 = new Player("Player2",2);
+		player3 = new Player("Player3",3);
+		player4 = new Player("Player4",4);
+		party.add(player1);
+		party.add(player2);
+		party.add(player3);
+		party.add(player4);
 	}
 	
 	@Test
@@ -29,6 +39,11 @@ public class ScorePanelTest {
 	@Test
 	public void testSize() {
 		assertEquals(new Dimension(0,0),panel.getSize());
+	}
+	
+	@Test
+	public void testUpdateTimer() {
+		panel.updateTimer(100);
 	}
 
 }
