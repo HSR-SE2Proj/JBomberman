3231c7214769f545ebcdd4cc286a3a532f16ea0d
Silvan Adrian <hallo@silvanadrian.ch>
2016-02-26 16:38:54 +0100

    MapTests

--- src/test/java/jbomberman/game/server/StandardMapTest.java
+++ src/test/java/jbomberman/game/server/StandardMapTest.java
@@ -2,9 +2,36 @@ package jbomberman.game.server;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import jbomberman.game.GameObjectType;
+import jbomberman.game.Party;
+import jbomberman.game.Player;
+import jbomberman.network.server.ServerNetwork;
+
 public class StandardMapTest {
+	private GameObjectManager om;
+	private ServerNetwork serverNetwork;
+	private Party party;
+	private StandardMap standmap;
+	private Player player1,player2,player3,player4;
+	@Before
+	public void setUp() {
+		om = new GameObjectManager();
+		serverNetwork = new ServerNetwork();
+		serverNetwork.connect("localhost");
+		party = new Party();
+		player1 = new Player("Player1",1);
+		player2 = new Player("Player2",2);
+		player3 = new Player("Player3",3);
+		player4 = new Player("Player4",4);
+		party.add(player1);
+		party.add(player2);
+		party.add(player3);
+		party.add(player4);
+	}
 	@Test
 	public void testStandardMap() {
 		char[][] map = new char[][]{
@@ -22,7 +49,32 @@ public class StandardMapTest {
 			{'#','3','X',' ',' ',' ',' ',' ',' ',' ','X','4','#'},
 			{'#','#','#','#','#','#','#','#','#','#','#','#','#'}
 	};
-		StandardMap standmap = new StandardMap();
+		standmap = new StandardMap();
 		assertArrayEquals(map,standmap.map);
+	}
+	
+	@Test
+	public void testInit() {
+		standmap = new StandardMap();
+		standmap.init(om, serverNetwork, party);
+		assertEquals(73,om.getByType(GameObjectType.SOLID_BLOCK).size());
+		assertEquals(4,om.getByType(GameObjectType.BOMBERMAN).size());
+		//Player1 Position
+		assertEquals(64,om.getById(1).getPosition().getX());
+		assertEquals(64,om.getById(1).getPosition().getY());
+		//Player2 Position
+		assertEquals(704,om.getById(2).getPosition().getX());
+		assertEquals(64,om.getById(2).getPosition().getY());
+		//Player3 Position
+		assertEquals(64,om.getById(3).getPosition().getX());
+		assertEquals(704,om.getById(3).getPosition().getY());
+		//Player4 Position
+		assertEquals(704,om.getById(4).getPosition().getX());
+		assertEquals(704,om.getById(4).getPosition().getY());
+	}
+	
+	@After
+	public void tearDown() {
+		serverNetwork.close();
 	}
 }
