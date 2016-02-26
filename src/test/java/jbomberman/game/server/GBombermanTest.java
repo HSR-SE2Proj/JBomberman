b2e64c224bb105d6d099eecd97505c15506273cf
Silvan Adrian <hallo@silvanadrian.ch>
2016-02-26 12:10:01 +0100

    more GBomberman Tests

--- src/test/java/jbomberman/game/server/GBombermanTest.java
+++ src/test/java/jbomberman/game/server/GBombermanTest.java
@@ -12,15 +12,22 @@ import jbomberman.game.Action;
 import jbomberman.game.ActionQueue;
 import jbomberman.game.ActionType;
 import jbomberman.game.BombermanState;
+import jbomberman.game.DirectionType;
+import jbomberman.game.PowerUpType;
+import jbomberman.network.server.ServerNetwork;
 import jbomberman.utils.Position;
 
 public class GBombermanTest {
 	private GBomberman gbomberman;
+	private GExplosion explosion;
+	private GPowerUp powerup;
 	private GSolidBlock solid;
+	private GBomb bomb;
 	private GameObjectManager om;
 	private Position pos;
 	private ActionQueue queue;
 	private Action action;
+	private ServerNetwork serverNetwork;
 	@Before
 	public void setUp() {
 		pos = new Position(64,64);
@@ -80,6 +87,112 @@ public class GBombermanTest {
 		assertEquals(BombermanState.IDLE_DOWN,gbomberman.getState());
 		assertEquals(64,gbomberman.getPosition().getX());
 		assertEquals(ActionType.CREATE_BOMB,queue.take().getActionType());
+	}
+	
+	@Test
+	public void testExplosion() {
+		gbomberman.setPosition(pos);
+		bomb = new GBomb(pos, 2, 3, 2);
+		om.add(bomb);
+		for (int i = 0; i < 91;i++){
+			bomb.tick(queue, om);
+		}
+		assertEquals(ActionType.DESTROY,queue.take().getActionType());
+		assertEquals(ActionType.CREATE_EXPLOSION,queue.take().getActionType());
+		assertEquals(ActionType.CREATE_EXPLOSION,queue.take().getActionType());
+		assertEquals(ActionType.CREATE_EXPLOSION,queue.take().getActionType());
+		assertEquals(ActionType.CREATE_EXPLOSION,queue.take().getActionType());
+		assertEquals(ActionType.CREATE_EXPLOSION,queue.take().getActionType());
+		//Create Explosion Manualy
+		explosion = new GExplosion(pos,3,4,DirectionType.DOWN);
+		om.add(explosion);
+		gbomberman.tick(om, queue);
+		assertEquals(BombermanState.DEAD,gbomberman.getState());
+		assertEquals(ActionType.DESTROY,queue.take().getActionType());
+	}
+	
+	@Test
+	public void testSetFullPowerUps() {
+		gbomberman.setFullPowerups();
+		gbomberman.setPosition(pos);
+		action = new Action(ActionType.PLAYER_INPUT, new Object[]{0, KeyEvent.VK_D, true});
+		gbomberman.update(action);
+		gbomberman.tick(om, queue);
+		assertEquals(BombermanState.RIGHT,gbomberman.getState());
+		assertEquals(72,gbomberman.getPosition().getX());
+	}
+	
+	@Test
+	public void testIdleState() {
+		serverNetwork = new ServerNetwork();
+		serverNetwork.connect("localhost");
+		action = new Action(ActionType.PLAYER_INPUT, new Object[]{0, KeyEvent.VK_D, true});
+		gbomberman.update(action);
+		gbomberman.tick(om, queue);
+		action = new Action(ActionType.PLAYER_INPUT, new Object[]{0, KeyEvent.VK_D, false});
+		gbomberman.update(action);
+		gbomberman.tick(om, queue);
+		gbomberman.sendUpdates(serverNetwork);
+		gbomberman.tick(om, queue);
+		assertEquals(BombermanState.IDLE_RIGHT,gbomberman.getState());
+		action = new Action(ActionType.PLAYER_INPUT, new Object[]{0, KeyEvent.VK_W, true});
+		gbomberman.update(action);
+		gbomberman.tick(om, queue);
+		action = new Action(ActionType.PLAYER_INPUT, new Object[]{0, KeyEvent.VK_W, false});
+		gbomberman.update(action);
+		gbomberman.tick(om, queue);
+		gbomberman.sendUpdates(serverNetwork);
+		gbomberman.tick(om, queue);
+		assertEquals(BombermanState.IDLE_UP,gbomberman.getState());
+		action = new Action(ActionType.PLAYER_INPUT, new Object[]{0, KeyEvent.VK_S, true});
+		gbomberman.update(action);
+		gbomberman.tick(om, queue);
+		action = new Action(ActionType.PLAYER_INPUT, new Object[]{0, KeyEvent.VK_S, false});
+		gbomberman.update(action);
+		gbomberman.tick(om, queue);
+		gbomberman.sendUpdates(serverNetwork);
+		gbomberman.tick(om, queue);
+		assertEquals(BombermanState.IDLE_DOWN,gbomberman.getState());
+		action = new Action(ActionType.PLAYER_INPUT, new Object[]{0, KeyEvent.VK_A, true});
+		gbomberman.update(action);
+		gbomberman.tick(om, queue);
+		action = new Action(ActionType.PLAYER_INPUT, new Object[]{0, KeyEvent.VK_A, false});
+		gbomberman.update(action);
+		gbomberman.tick(om, queue);
+		gbomberman.sendUpdates(serverNetwork);
+		gbomberman.tick(om, queue);
+		assertEquals(BombermanState.IDLE_LEFT,gbomberman.getState());
+	}
+	
+	@Test
+	public void testPowerUp() {
+		//Bomb
+		powerup = new GPowerUp(pos, 2, PowerUpType.BOMB);
+		om.add(powerup);
+		gbomberman.tick(om, queue);
+		om.remove(2);
+		assertEquals(ActionType.DESTROY,queue.take().getActionType());
+		//Power
+		powerup = new GPowerUp(pos, 3, PowerUpType.POWER);
+		om.add(powerup);
+		gbomberman.tick(om, queue);
+		action = new Action(ActionType.PLAYER_INPUT, new Object[]{0, KeyEvent.VK_ENTER, true});
+		gbomberman.update(action);
+		om.remove(3);
+		gbomberman.tick(om, queue);
+		assertEquals(ActionType.DESTROY,queue.take().getActionType());
+		assertEquals(1,queue.take().getProperty(2));
+		//Speed
+		powerup = new GPowerUp(pos, 4, PowerUpType.SPEED);
+		om.add(powerup);
+		gbomberman.tick(om, queue);
+		om.remove(4);
+		assertEquals(ActionType.DESTROY,queue.take().getActionType());
+		action = new Action(ActionType.PLAYER_INPUT, new Object[]{0, KeyEvent.VK_D, true});
+		gbomberman.update(action);
+		gbomberman.tick(om, queue);
+		assertEquals(BombermanState.RIGHT,gbomberman.getState());
+		assertEquals(72,gbomberman.getPosition().getX());
 	}
 	
 }
